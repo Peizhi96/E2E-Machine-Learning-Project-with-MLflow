1c59f78bfd3a0c2f344b5605e427d51dbc591d5b
Peizhi Yan <yanzhenyi123@gmail.com>
2024-03-27 22:02:35 -0400

    data validation added

---
+++ src/ml_project/pipeline/stage_02_data_validation.py
@@ -0,0 +1,23 @@
+from ml_project.config.configuration import ConfigurationManager
+from ml_project.components.data_validation import DataValidation
+from ml_project import logger
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+    
+if __name__ == "__main__":
+    try:
+        logger.info(">>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(">>>>> stage {STAGE_NAME} completed <<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
